1b4343345a0ea8db5b57b533a766d66d9ac7bee5
Raymond Oeta <oetaray@gmail.com>
2023-08-14 21:46:51 +0300

    completed work

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,27 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
+    
 
-def greet(name):
+def greet(name = 'Guest'):
     pass
+    print('Hello, {name}!')
+
 
 def greet_with_default(name="programmer"):
     pass
+    print('Hi there, {name}')
 
+    
 def add(num1, num2):
     pass
+    return num1 + num2
+add(34, 35)
+# 69
 
 def halve(number):
     pass
+    return number / 2
+halve(420)
+#420
